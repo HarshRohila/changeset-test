7c6f0a8bb334f471d53cfa0a51e454cab0f44a93
Harsh Rohila <rohilaharsh@gmail.com>
2020-06-17 22:32:38 +0530

    renamed project

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'chainset-test',
+    modulePrefix: 'changeset-test',
     environment,
     rootURL: '/',
     locationType: 'auto',
