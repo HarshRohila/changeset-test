2d50b026815ebebd5e66f7278b8addfca87fb606
Harsh Rohila <rohilaharsh@gmail.com>
2020-06-18 10:02:17 +0530

    added user form

---
+++ app/user/model.js
@@ -0,0 +1,9 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class UserModel extends Model {
+    @attr('string') name;
+    @attr('string') email;
+    @attr('number') age;
+    @attr('string') password;
+    @attr('string') confirmPassword;
+}
