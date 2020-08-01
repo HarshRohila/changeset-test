40b26c2afb6ae142e3aa62707ae82f96409567af
Harsh Rohila <rohilaharsh@gmail.com>
2020-08-01 21:54:45 +0530

    renamed todo component

--- tests/integration/components/todo-page/component-test.js
+++ tests/integration/components/todo-page/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | todo', function(hooks) {
+module('Integration | Component | todo-page', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Todo />`);
+    await render(hbs`<TodoPage />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <Todo>
+      <TodoPage>
         template block text
-      </Todo>
+      </TodoPage>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
