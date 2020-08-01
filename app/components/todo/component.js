c112ae69832958b4450f1f59562cff2fbe66e1b0
Harsh Rohila <rohilaharsh@gmail.com>
2020-08-01 21:49:33 +0530

    renamed todo component

--- app/components/todo/component.js
+++ app/components/todo/component.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-export default class ApplicationPageComponent extends Component {
+export default class TodoComponent extends Component {
 
   @service store;
   @tracked message = '';
